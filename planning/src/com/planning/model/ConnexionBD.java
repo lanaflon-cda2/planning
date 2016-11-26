bfe7f62bf3d7c4f14c65a4234cec7f783f2aa661
wilofice <wilofice@gmail.com>
2016-11-26 12:07:45 +0000

    Mise au point des Classes DAO. Implémentation de la methode permettant d'inserer pour un créneau Statique, toutes les séances correspondantes au Cours de l'année.

--- planning/src/com/planning/model/ConnexionBD.java
+++ planning/src/com/planning/model/ConnexionBD.java
@@ -15,7 +15,7 @@ public class ConnexionBD {
         Connection conn = null;
         try{
             Class.forName("com.mysql.jdbc.Driver");
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/planning_bd_test","root","root");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/planning_bd","root","root");
             if (conn != null)
                 System.out.println("Connexion Reussite");
             else
